c2d099a0e470bc578b32558722227815e94f82b7
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2016-01-19 00:20:40 -0500

    Poner la dirección de app/console de forma absoluta para que no falle con supervisor

--- CULabsIlluminateBundle.php
+++ CULabsIlluminateBundle.php
@@ -6,14 +6,13 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\Console\Application;
 use CULabs\IlluminateBundle\Bridge\Console\Application as BridgeApplication;
 
-if (!defined('ARTISAN_BINARY')) {
-    define('ARTISAN_BINARY', 'app/console');
-}
-
 class CULabsIlluminateBundle extends Bundle
 {
     public function registerCommands(Application $application)
     {
+        if (!defined('ARTISAN_BINARY')) {
+            define('ARTISAN_BINARY', $this->container->getParameter('kernel.root_dir').'/console');
+        }
         parent::registerCommands($application);
         $app = $this->container->get('cu_labs_illuminate.container');
         $bridgeApplication = new BridgeApplication($app, $app['events'], 'laravel', $application);
